960ff9f13c88b00d765039be0769371343dd0612
Pierre Zhang <chromasetha@outlook.com>
2024-08-07 11:10:21 +0800

    Create zh_CN.lua

---
+++ localization/zh_CN.lua
@@ -0,0 +1,16 @@
+return {
+	["misc"] = {
+		["dictionary"] = {
+			["gald_master"] = "启用Galdur",
+            ["gald_anim"] = "启用动画",
+            ["gald_select_deck"] = "选择牌组",
+            ["gald_select_stake"] = "选择赌注",
+            ["gald_play"] = "开始游戏",
+            ["gald_selected"] = "已选择",
+            ["gald_locked"] = "锁定",
+            ["gald_unlock_1"] = "在",
+            ["gald_unlock_and"] = "和",
+            ["gald_unlock_2"] = "中赢得赛局以解锁此赌注",
+        }
+    }
+}
