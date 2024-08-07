6190ca3baaf4ba9cefa11617e5ad3142831d8baf
Eremel <richardbailey@outlook.com>
2024-08-07 23:36:18 +0100

    localization update

--- localization/default.lua
+++ localization/default.lua
@@ -3,24 +3,21 @@ return {
 		["dictionary"] = {
 			["gald_master"] = "Enable Galdur",
             ["gald_use_desc"] = {
-                "Toggle the use of the",
-                "Galdur setup"    
+                "Toggle the use of the Galdur setup"    
             },
             ["gald_anim"] = "Enable animations",
             ["gald_anim_desc"] = {
-                "This option enables the",
-                "animations of the deck",
-                "preview and chip tower"    
+                "This option enables the animations",
+                "of the deck preview and chip tower"    
             },
             ["gald_reduce"] = "Reduce deck size",
             ["gald_reduce_desc"] = {
-                "This option reduces the",
-                "number of cards on the screen"    
+                "This option reduces the number",
+                "of cards on the screen"    
             },
             ["gald_unlock"] = "Unlock all stakes",
             ["gald_unlock_desc"] = {
-                "This option sets all",
-                "stakes as playable"    
+                "This option sets all stakes as playable"    
             },
             ["gald_select_deck"] = "Select Deck",
             ["gald_select_stake"] = "Select Stake",
@@ -29,7 +26,6 @@ return {
             ["gald_locked"] = "Locked",
             ["gald_unlock_1"] = "Win with this deck on ",
             ["gald_unlock_and"] = " and ",
-            ["gald_unlock_2"] = "to unlock this stake",
         }
     }
 }
