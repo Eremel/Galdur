9e9881849b26e6ed794394341635bd3902df8c97
Marffe <efframlol@gmail.com>
2025-09-01 22:46:11 -0500

    Spanish Translation

---
+++ localization/es_419.lua
@@ -0,0 +1,56 @@
+return {
+	["misc"] = {
+		["dictionary"] = {
+			["gald_master"] = "Desactivar Galdur",
+            ["gald_use_desc"] = {
+                "Esta Opción desactiva la",
+                "Pantalla de inicio de Galdur"
+            },
+            ["gald_anim"] = "Activar Animaciones",
+            ["gald_anim_desc"] = {
+                "Activa las animaciones de las",
+                "Barajas y los Pozos"    
+            },
+            ["gald_reduce"] = "Mazo pequeño",
+            ["gald_reduce_desc"] = {
+                "Reduce las cantidad de",
+                "cartas en pantalla de selección"    
+            },
+            ["gald_stake_colour"] = "Oscurecer Pozos",
+            ["gald_stake_colour_options"] = {
+                "Oscurecer incompletos",    
+                "Oscurecer completados",
+            },
+            ["gald_stake_colour_desc"] = {
+                "Selecciona que tipo de",
+                "Pozo se oscurecer en la",
+                "pantalla de selección"
+            },
+            ["gald_stake_select"] = "Selección de Pozo",
+            ["gald_stake_select_options"] = {
+                "Último Juego",    
+                "Próximo Desbloqueo",
+                "Pozo Blanco"
+            },
+            ["gald_stake_select_desc"] = {
+                "Selecciona que Pozo será seleccionado",
+                "por defecto en la pantalla de selección"
+            },
+            ["gald_unlock"] = "Desbloquear Pozos",
+            ["gald_unlock_desc"] = {
+                "Desbloquea todos los Pozos"   
+            },
+            ["gald_select_deck"] = "Escoger Baraja",
+            ["gald_select_stake"] = "Escoger Pozo",
+            ["gald_random_deck"] = "Al azar",
+	        ["gald_random_stake"] = "Al azar",
+            ["gald_play"] = "Jugar",
+            ["gald_selected"] = "Seleccionado",
+            ["gald_locked"] = "Bloqueado",
+            ["gald_unlock_1"] = "Gana con esta Baraja en el ",
+            ["gald_unlock_and"] = " y ",
+            ["gald_new_page_error"] = "Erro al añadir página",
+            ["gald_quick_start"] = "Partida Rápida"
+        }
+    }
+}
